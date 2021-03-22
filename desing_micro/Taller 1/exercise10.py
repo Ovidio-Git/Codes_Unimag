da8cee5cb575a6ce5ca4a6c59064ae50b91876f8
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-21 21:40:32 -0500

    first commit ex10

---
+++ desing_micro/Taller 1/exercise10.py
@@ -0,0 +1,21 @@
+
+def funtion(x,y):
+
+    a = [ 2*x , 50*y ]
+    return a
+
+def funtion2 (a):
+    x = a - alpha * b
+
+def steepest_descent():
+    x0 = 0.5
+    y0 = 0.5
+    alpha = 0.01
+    print(funtion(x0,y0))
+
+def run():
+    steepest_descent()
+
+
+if __name__ == '__main__':
+    run()
