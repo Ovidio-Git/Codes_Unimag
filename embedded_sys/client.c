4915f715d34dd1f6cc302a4430a65f403f5c927c
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-04-12 13:35:51 -0500

    server 20%

--- embedded_sys/client.c
+++ embedded_sys/client.c
@@ -14,7 +14,7 @@ UDP = information fast
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h> // socket library
+#include <sys/socket.h> //for socket(), connect(), send() and recv()
 #include <netinet/in.h>
 
 // SOCK_STREAM -> TCP 
@@ -24,25 +24,26 @@ int main(){
     
     int connection = 0;
     int socket_id = 0;
-    char msg[] ="Hello sir\n\r";
+    char msg[] ="Hello sir, message send scket instance\n\r";
     socket_id = socket(PF_INET, SOCK_STREAM, 0);
 
     // Server parameters   AWS IP Public= 18.222.148.97
-    struct sockaddr_in server;
-    server.sin_family = AF_INET ;  // protocol
-    server.sin_addr.s_addr = inet_addr("18.222.148.97"); // server ip
-    server.sin_port = htons(12000); // connection port
+    struct sockaddr_in serverc;
+    serverc.sin_family = AF_INET ;  // protocol
+    serverc.sin_addr.s_addr = inet_addr("18.222.148.97"); // server ip
+    serverc.sin_port = htons(19900); // connection port
     // htons(port number) convert to 6 bit format 
     
     
     // Make connection
-    connection = connect(socket_id, (struct sockaddr*)&server, sizeof(server));
+    connection = connect(socket_id, (struct sockaddr*)&serverc, sizeof(serverc));
     if (connection < 0){
         printf("[ERROR] connection failed\r\n");
         return(-1);
     }
     printf("[CONNECTION SUCCESS]\r\n");
     send(socket_id, msg, sizeof(msg), 0); 
+    printf("[DATA SEND]\r\n");
     close(socket_id);
 
     return (0);
