1100888405d30c5253c12e302db9bba373f3e01d
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-05-09 02:20:06 -0500

    i found the words for post form

--- embedded_sys/search.c
+++ embedded_sys/search.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-char search(char chain[], char target[]){
+void search(char chain[], char target[]){
 
-    int n = 0;
-    int m = 0;
-    int len = strlen(target);
+    int  n = 0;
+    char m = 0;
+    char i = 0;
+    char len = strlen(target);
     char find[10];
-    int j = 0;
 
 
     while (chain[n] != '\0'){
-        printf("2");
         if (chain[n]==target[m]){
-            printf("3");
             while(chain[n] == target[m]){
                 n++;
                 m++;
-                printf("4");
                 if (m==len){
-                    printf("5");
-                    while(chain[n] != '\0'){
-                        printf("6");
-                        find[j]=chain[n];
-                        j++;
+                    while(chain[n] != ' ' && chain[n] != '\n'){
+                        find[i]=chain[n];
+                        i++;
                         n++;
                     }
+                    break;
                 }
             }
             m=0;
         }
         n++;
-        m=0;
     }
-    printf("7");
+    for (int i =0; i <= 8; i++){
+        printf("%c", find[i]);
+    }
 }
 
 
 int main(){
     char b[]="Accept-Language: en-US,en;q=0.9  Username=er32 Password=89re ccep� ";
-    char t[]="Usernamee";
+    char t[]="Username=";
     search(b, t);
+    printf("7");
 }
